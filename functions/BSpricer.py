d5d7814f81cf711c5be3ff34eb5159837472407f
ornella-el <ornella272@gmail.com>
2023-06-24 19:12:34 +0200

    Put and call options based calibration BS model

---
+++ functions/BSpricer.py
@@ -0,0 +1,85 @@
+import numpy as np
+import pandas as pd
+import scipy.stats as ss
+
+""" 
+PARAMETERS
+S0: the current price of the underlying asset
+r: the risk-free interest rate
+sigma: the diffusion coefficient
+K: the strike price of the option
+ttm: time-to maturity
+exercise: 
+type: 'call' or 'put'
+????option_price: the call (or put) price given by the column 'Last Price' in df 
+"""
+
+
+class BS_pricer:
+    """
+    Closed Formula.
+    """
+
+    def __init__(self, S0, r, sigma,  ttm, exercise, K, type_o):
+        self.S0 = S0  # current price
+        self.r = r  # interest rate
+        self.sigma = sigma  # diffusion coefficient
+        self.K = None  # strike
+        self.ttm = ttm  # maturity in years
+        # self.price = 0
+        self.exercise = exercise
+        self.type_o = type_o if type_o is not None else 'no_type'
+
+    def payoff_f(self, St):
+        if self.type_o == 'call':
+            payoff = np.maximum(St - self.K, 0)
+        elif self.type_o == 'put':
+            payoff = np.maximum(self.K - St, 0)
+        else:
+            raise ValueError('Please select "call" or "put" type.')
+        return payoff
+
+    def d1_f(self):
+        return (np.log(self.S0 / self.K) + ((self.r + 0.5 * self.sigma ** 2) * self.ttm)) / (
+                self.sigma * np.sqrt(self.ttm) + 1e-6)
+
+    def d2_f(self):
+        return self.d1_f() - self.sigma * np.sqrt(self.ttm)
+
+    @staticmethod
+    def vega(self):
+        """ BS vega: derivative of the price with respect to the volatility """
+        return self.S0 * np.sqrt(self.ttm) * ss.norm.pdf(self.d1_f())
+
+    @staticmethod
+    def BlackScholes(type_o='call', S0=100., K=100., ttm=1., r=0.1, sigma=0.2):
+        """ Black Scholes closed formula:
+            type_o: call or put.
+            S0: float.    initial stock/index level.
+            K: float strike price.
+            ttm: float time-to-maturity (in year fractions).
+            r: float constant risk-free short rate.
+            sigma: volatility factor in diffusion term. """
+
+        d1 = (np.log(S0 / K) + (r + sigma ** 2 / 2) * ttm) / (sigma * np.sqrt(ttm))
+        d2 = d1 - (sigma * np.sqrt(ttm))
+
+        if type_o == "call":
+            return S0 * ss.norm.cdf(d1) - K * np.exp(-r * ttm) * ss.norm.cdf(d2)
+        elif type_o == "put":
+            return K * np.exp(-r * ttm) * ss.norm.cdf(-d2) - S0 * ss.norm.cdf(-d1)
+        else:
+            raise ValueError("invalid type. Set 'call' or 'put'")
+
+    def closed_formula(self, K):
+        """
+        Black Scholes closed formula:
+        """
+        self.K = K
+
+        if self.type_o == 'call':
+            return self.S0 * ss.norm.cdf(self.d1_f()) - (self.K * np.exp(-self.r * self.ttm) * ss.norm.cdf(self.d2_f()))
+        elif self.type_o == 'put':
+            return self.K * np.exp(-self.r * self.ttm) * ss.norm.cdf(- self.d2_f()) - self.S0 * ss.norm.cdf(- self.d1_f())
+        else:
+            raise ValueError('Please select "call" or "put" type.')
---
+++ functions/plainvanilla.py
@@ -0,0 +1,29 @@
+import pandas as pd
+
+df = pd.read_csv('data/spx_vanilla_201601.csv')
+print(df)
+print(df.columns)
+
+# Select rows where 'QUOTE_DATE' is equal to '2016-01-20' and 'EXPIRE_DATE' is equal to '2017-01-20
+cond1 = df['QUOTE_DATE'] == '2016-01-20'
+cond2 = df['EXPIRE_DATE'] == '2017-01-20'
+
+df_filtered = df[cond1 & cond2].copy()
+
+# Select only 20 options to use for calibration (STRIKE around UNDERLYING_LAST)
+cond3 = df_filtered['STRIKE_DISTANCE'] <= 170.0
+
+df_filtered = df_filtered[cond3]
+
+# Create a separate dataframe for call and put options
+df_call = df_filtered[['QUOTE_DATE', 'UNDERLYING_LAST', 'EXPIRE_DATE', 'C_BID', 'C_ASK', 'STRIKE']].copy()
+print(df_call)
+
+df_put = df_filtered[['QUOTE_DATE', 'UNDERLYING_LAST', 'EXPIRE_DATE', 'P_BID', 'P_ASK', 'STRIKE']].copy()
+print(df_put)
+print(df_call.shape, df_put.shape)
+
+# export to a CSV file
+df_call.to_csv('data/options_spx_call_2016.csv', index=False)
+df_put.to_csv('data/options_spx_put_2016.csv', index=False)
+
