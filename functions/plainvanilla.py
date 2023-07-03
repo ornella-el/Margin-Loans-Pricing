2e937c39b46151f3771368c3398da06b86983fc2
ornella-el <ornella272@gmail.com>
2023-07-03 16:40:17 +0200

    Elaborate source data

--- functions/plainvanilla.py
+++ functions/plainvanilla.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('data/spx_vanilla_201601.csv')
+df = pd.read_csv('../data/spx_vanilla_1601.csv')
 print(df)
 print(df.columns)
 
@@ -11,7 +11,7 @@ cond2 = df['EXPIRE_DATE'] == '2017-01-20'
 df_filtered = df[cond1 & cond2].copy()
 
 # Select only 20 options to use for calibration (STRIKE around UNDERLYING_LAST)
-cond3 = df_filtered['STRIKE_DISTANCE'] <= 170.0
+cond3 = df_filtered['STRIKE_DISTANCE'] <= 1200.0
 
 df_filtered = df_filtered[cond3]
 
@@ -24,6 +24,5 @@ print(df_put)
 print(df_call.shape, df_put.shape)
 
 # export to a CSV file
-df_call.to_csv('data/options_spx_call_2016.csv', index=False)
-df_put.to_csv('data/options_spx_put_2016.csv', index=False)
-
+df_call.to_csv('../data/options_spx_call_2016.csv', index=False)
+df_put.to_csv('../data/options_spx_put_2016.csv', index=False)
