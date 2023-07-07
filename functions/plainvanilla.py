a2ad381d716a424eac44a76ef7c5d5ccb422b584
ornella-el <ornella272@gmail.com>
2023-07-07 14:06:39 +0200

    Implied volatility adjusted

--- functions/plainvanilla.py
+++ functions/plainvanilla.py
@@ -11,15 +11,15 @@ cond2 = df['EXPIRE_DATE'] == '2017-01-20'
 df_filtered = df[cond1 & cond2].copy()
 
 # Select only 20 options to use for calibration (STRIKE around UNDERLYING_LAST)
-cond3 = df_filtered['STRIKE_DISTANCE'] <= 1200.0
+cond3 = df_filtered['STRIKE_DISTANCE'] <= 1000.0
 
 df_filtered = df_filtered[cond3]
 
 # Create a separate dataframe for call and put options
-df_call = df_filtered[['QUOTE_DATE', 'UNDERLYING_LAST', 'EXPIRE_DATE', 'C_BID', 'C_ASK', 'STRIKE']].copy()
+df_call = df_filtered[['QUOTE_DATE', 'UNDERLYING_LAST', 'EXPIRE_DATE', 'C_BID', 'C_ASK', 'STRIKE', 'C_IV']].copy()
 print(df_call)
 
-df_put = df_filtered[['QUOTE_DATE', 'UNDERLYING_LAST', 'EXPIRE_DATE', 'P_BID', 'P_ASK', 'STRIKE']].copy()
+df_put = df_filtered[['QUOTE_DATE', 'UNDERLYING_LAST', 'EXPIRE_DATE', 'P_BID', 'P_ASK', 'STRIKE', 'P_IV']].copy()
 print(df_put)
 print(df_call.shape, df_put.shape)
 
