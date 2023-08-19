d7d19267975f173e2f36e3d37f36d8b0fcf42729
ornella-el <ornella272@gmail.com>
2023-08-19 12:42:18 +0200

    stocks data organized

--- functions/plainvanilla.py
+++ functions/plainvanilla.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-df = pd.read_csv('../data/spx_vanilla_1601.csv')
+df = pd.read_csv('../data/nvda_vanilla_1601.csv')
 print(df)
 print(df.columns)
 
@@ -14,25 +14,53 @@ df_filtered = df[cond1 & cond2].copy()
 S0 = df['UNDERLYING_LAST'][0]
 bounds = [S0 * 0.5, S0 * 1.5]
 cond3 = (df_filtered['STRIKE'] >= bounds[0]) & (df_filtered['STRIKE'] <= bounds[1])
-df_filtered = df_filtered[cond3]
+df_filtered1 = df_filtered[cond3].copy
+
+# Select only options whose moneyness K/S0 is in range [0.75, 1.35]
+df_filtered['MONEYNESS'] = round(df_filtered['STRIKE'] / df_filtered['UNDERLYING_LAST'],2)
+
+print(df_filtered)
+
+# Select rows where moneyness is in the desired range
+moneyness_cond2 = (df_filtered['MONEYNESS'] >= 0.75) & (df_filtered['MONEYNESS'] <= 1.35)
+moneyness_cond3 = (df_filtered['MONEYNESS'] >= 0.5) & (df_filtered['MONEYNESS'] <= 1.5)
+
+df_filtered2 = df_filtered[moneyness_cond2]
+print(df_filtered2)
+df_filtered3 = df_filtered[moneyness_cond3]
+print(df_filtered3)
 
 # Create a separate dataframe for call and put options
-df_call = df_filtered[['QUOTE_DATE', 'UNDERLYING_LAST', 'EXPIRE_DATE', 'C_BID', 'C_ASK', 'STRIKE', 'C_IV']].copy()
-print(df_call)
-df_call.to_csv('../data/spx_basket_calls_2016.csv', index=False)
+df_calls_bounds = df_filtered[['QUOTE_DATE', 'UNDERLYING_LAST', 'EXPIRE_DATE', 'C_BID', 'C_ASK', 'STRIKE', 'C_IV']].copy()
+print(df_calls_bounds)
 
-df_call_otm = df_call[df_call['STRIKE'] > df_call['UNDERLYING_LAST']]
+# 1. Dataframe with bounds on the STRIKE price
+df_puts_bounds = df_filtered[['QUOTE_DATE', 'UNDERLYING_LAST', 'EXPIRE_DATE', 'P_BID', 'P_ASK', 'STRIKE', 'P_IV']].copy()
+print(df_puts_bounds)
+print(df_calls_bounds.shape, df_puts_bounds.shape)
 
-df_put = df_filtered[['QUOTE_DATE', 'UNDERLYING_LAST', 'EXPIRE_DATE', 'P_BID', 'P_ASK', 'STRIKE', 'P_IV']].copy()
-print(df_put)
-print(df_call.shape, df_put.shape)
+# 2. Dataframe with only OTM options
+df_call_otm = df_calls_bounds[df_calls_bounds['STRIKE'] > df_calls_bounds['UNDERLYING_LAST']]
+df_put_otm = df_puts_bounds[df_puts_bounds['STRIKE'] < df_puts_bounds['UNDERLYING_LAST']]
 
-df_put.to_csv('../data/spx_basket_puts_2016.csv', index=False)
 
-df_put_otm = df_put[df_put['STRIKE'] < df_put['UNDERLYING_LAST']]
+# 3. Dataframe with MONEYNESS constraint [0.75, 1.35]
+df_calls_moneyness2 = df_filtered2[['QUOTE_DATE', 'UNDERLYING_LAST', 'EXPIRE_DATE', 'C_BID', 'C_ASK', 'STRIKE', 'C_IV', 'MONEYNESS']].copy()
+df_puts_moneyness2 = df_filtered2[['QUOTE_DATE', 'UNDERLYING_LAST', 'EXPIRE_DATE', 'P_BID', 'P_ASK', 'STRIKE', 'P_IV', 'MONEYNESS']].copy()
+print(df_calls_moneyness2, df_calls_moneyness2.shape)
+
+# 3. Dataframe with MONEYNESS constraint [0.5, 1.35]
+df_calls_moneyness3 = df_filtered3[['QUOTE_DATE', 'UNDERLYING_LAST', 'EXPIRE_DATE', 'C_BID', 'C_ASK', 'STRIKE', 'C_IV', 'MONEYNESS']].copy()
+df_puts_moneyness3 = df_filtered3[['QUOTE_DATE', 'UNDERLYING_LAST', 'EXPIRE_DATE', 'P_BID', 'P_ASK', 'STRIKE', 'P_IV', 'MONEYNESS']].copy()
+print(df_calls_moneyness3, df_calls_moneyness3.shape)
 
 # export to a CSV file
-df_call.to_csv('../data/options_spx_call_2016.csv', index=False)
-df_call_otm.to_csv('../data/options_spx_call_OTM_2016.csv', index=False)
-df_put.to_csv('../data/options_spx_put_2016.csv', index=False)
-df_put_otm.to_csv('../data/options_spx_put_OTM_2016.csv', index=False)
+
+# df_calls_bounds.to_csv('../data/spx_basket_calls_2016.csv', index=False)
+# df_puts_bounds.to_csv('../data/spx_basket_puts_2016.csv', index=False)
+df_call_otm.to_csv('../data/nvda_calls_OTM_2016.csv', index=False)
+df_put_otm.to_csv('../data/nvda_puts_OTM_2016.csv', index=False)
+df_calls_moneyness2.to_csv('../data/OPT16_NVDA_CALLS_75_135.csv', index=False)
+df_puts_moneyness2.to_csv('../data/OPT16_NVDA_PUTS_75_135.csv', index=False)
+df_calls_moneyness3.to_csv('../data/OPT16_NVDA_CALLS_50_150.csv', index=False)
+df_puts_moneyness3.to_csv('../data/OPT16_NVDA_PUTS_50_150.csv', index=False)
